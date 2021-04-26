2d7c12b007bb73f9e2c194b76204c138ce3776d9
Jean-Marc Chapuzot <jean-marc.chapuzot@t-online.de>
2021-04-26 11:37:46 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WearShorts
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
